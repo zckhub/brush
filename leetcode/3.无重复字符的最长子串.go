9c81afc52cc6f6faac2ac8a43bd8e4f3d8be119a
zckhub <515588290@qq.com>
2024-08-07 00:29:24 +0800

    file merge

--- leetcode/3.无重复字符的最长子串.go
+++ leetcode/3.无重复字符的最长子串.go
@@ -1,11 +1,28 @@
 package main
 
+import "fmt"
+
+func main() {
+	res := lengthOfLongestSubstring("dvdf")
+	fmt.Println(res)
+}
 func lengthOfLongestSubstring(s string) int {
 	help := map[byte]int{}
-	left, right, res := 0
-	for index, value := range s {
-		for right < len(s) {
-
+	left, right, res, tmpRes := 0, 0, 0, 0
+	for right <= len(s)-1 {
+		if right <= len(s)-1 && help[s[right]] == 0 {
+			help[s[right]]++
+			right++
+			tmpRes++
+			if res <= tmpRes {
+				res = tmpRes
+			}
+		} else {
+			help[s[left]]--
+			left++
+			tmpRes--
 		}
+
 	}
+	return res
 }
