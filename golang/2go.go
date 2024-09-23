28ee06a7c3b29a87a417550089c83bc8c3aff007
zckhub <515588290@qq.com>
2024-09-23 23:42:24 +0800

    345

--- golang/2go.go
+++ golang/2go.go
@@ -7,13 +7,15 @@ import (
 	"time"
 )
 
-//写代码实现两个 goroutine，其中一个产生随机数并写入到 go channel 中，另外一个从 channel 中读取数字并打印到标准输出。最终输出五个随机数。
+/*写代码实现两个 goroutine，其中一个产生随机数并写入到 go channel 中，
+另外一个从 channel 中读取数字并打印到标准输出。最终输出五个随机数。*/
 func main() {
 	ch := make(chan int)
 	var wg sync.WaitGroup
-	wg.Add(10)
 	for i := 0; i < 5; i++ {
+		wg.Add(1)
 		go GenNum(ch, &wg)
+		wg.Add(1)
 		go FmtNum(ch, &wg)
 		time.Sleep(1 * time.Second)
 
